b5d342dc028250bfd5e224d733dfac4210820b48
omarreiss <omar.reiss@gmail.com>
2020-06-16 19:29:39 +0200

    Add internal linking block category.

---
+++ src/integrations/blocks/internal-linking-category.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * WPSEO plugin file.
+ *
+ * @package Yoast\WP\SEO\Integrations\Blocks
+ */
+namespace Yoast\WP\SEO\Integrations\Blocks;
+
+use Yoast\WP\SEO\Integrations\Integration_Interface;
+
+/**
+ * Dynamic blocks category class
+ */
+class Internal_Linking_Category implements Integration_Interface {
+	/**
+	 * @inheritDoc
+	 */
+	public static function get_conditionals() {
+		return [];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function register_hooks() {
+		add_filter( 'block_categories', [ $this, 'add_block_category' ] );
+	}
+
+	/**
+	 * Adds block category for internal linking blocks.
+	 *
+	 * @param array $categories The categories.
+	 * @return array The filtered categories.
+	 */
+	public function add_block_category( $categories ) {
+		$categories[] = [
+			'slug'  => 'yoast-internal-linking-blocks',
+			'title' => sprintf(
+				/* translators: %1$s expands to Yoast. */
+				__( '%1$s Internal Linking Blocks', 'wordpress-seo' ),
+				'Yoast'
+			),
+		];
+
+		return $categories;
+	}
+}
